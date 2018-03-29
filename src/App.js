96ab2aca367ee82a664a807c31e58f30b5633e09
Evans Musomi <evans.musomi@gmail.com>
2018-03-29 12:12:39 +0300

    Adds Rank Component and Application Styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Logo from "./components/Logo/Logo";
 import Navigation from "./components/Navigation/Navigation";
+import Particles from "react-particles-js";
+import Rank from "./components/Rank/Rank";
 import "./App.css";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 700
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles" params={particleOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*
         <FaceRecognition />*/}
