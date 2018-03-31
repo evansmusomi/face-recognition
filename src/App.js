043f3e591541a2745764d2be96c150a19a7049cd
Evans Musomi <evans.musomi@gmail.com>
2018-03-31 05:06:08 +0300

    Connects Components to Backend Server

--- src/App.js
+++ src/App.js
@@ -34,7 +34,14 @@ class App extends Component {
       imageUrl: "",
       box: {},
       route: "signin",
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        entries: "",
+        email: "",
+        id: "",
+        joined: "",
+        name: ""
+      }
     };
   }
 
@@ -56,17 +63,45 @@ class App extends Component {
     this.setState({ box });
   };
 
+  loadUser = data => {
+    this.setState({
+      user: {
+        entries: data.entries,
+        email: data.email,
+        id: data.id,
+        joined: data.joined,
+        name: data.name
+      }
+    });
+    console.log(this.state.user);
+  };
+
   onInputChange = event => {
     this.setState({ input: event.target.value });
   };
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     clarifaiApp.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response =>
-        this.displayFaceBox(this.calculateFaceLocation(response))
-      )
+      .then(response => {
+        if (response) {
+          fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/image`, {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            })
+            .catch(console.log);
+        }
+
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
       .catch(error => console.log(error));
   };
 
@@ -92,17 +127,23 @@ class App extends Component {
         {route === "home" ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit}
+              onPictureSubmit={this.onPictureSubmit}
             />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
         ) : route === "register" ? (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         ) : (
-          <Signin onRouteChange={this.onRouteChange} />
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         )}
       </div>
     );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
   return (
     <div>
       <p className="f3">
@@ -19,7 +19,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             placeholder="Paste Image Link Here..."
           />
           <button
-            onClick={onButtonSubmit}
+            onClick={onPictureSubmit}
             className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
           >
             Detect
